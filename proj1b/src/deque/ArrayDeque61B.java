eccf57f41345d8838e4b30ce27a21fc3308a6039
shizhenneko <2814911309@qq.com>
2025-11-26 13:33:06 +0800

    proj1b is finished

---
+++ proj1b/src/deque/ArrayDeque61B.java
@@ -0,0 +1,129 @@
+package deque;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayDeque61B<T> implements Deque61B<T> {
+    private int size;
+    private int nextfirst;
+    private int nextlast;
+    T[] deque = (T [])(new Object[8]);
+    public ArrayDeque61B(){
+    size = 0;
+    nextfirst = 0;
+    nextlast = 1;
+    }
+    public void reshape(){
+        if(size == deque.length){
+            T[] new_deque = (T [])(new Object[size*2]);
+            int first = Math.floorMod(nextfirst + 1, deque.length);
+            int firstPartSize = deque.length - first;
+            System.arraycopy(deque, first, new_deque, 0, firstPartSize);
+            System.arraycopy(deque, 0, new_deque, firstPartSize, nextlast);
+            deque = new_deque;
+            nextfirst = deque.length - 1;
+            nextlast = size;
+        }
+        else{
+            return;
+        }
+    }
+    public void small(){
+        if((double) size /deque.length<=0.25 && deque.length>=16){
+            T[] new_deque = (T [])(new Object[size/2]);
+            for(int i=0;i<size;i++)
+            {
+                new_deque[i] = get(i);
+            }
+            deque = new_deque;
+            nextfirst = deque.length-1;
+            nextlast = size;
+        }
+        else{
+            return;
+        }
+    }
+    @Override
+    public void addFirst(T x) {
+    reshape();
+    deque[Math.floorMod(nextfirst,deque.length)] = x;
+    nextfirst = Math.floorMod(nextfirst-1,deque.length);
+    size+=1;
+    return;
+    }
+
+    @Override
+    public void addLast(T x) {
+    reshape();
+    deque[Math.floorMod(nextlast,deque.length)] = x;
+    nextlast = Math.floorMod(nextlast+1,deque.length);
+    size+=1;
+    return;
+    }
+
+    @Override
+    public List<T> toList() {
+        List<T> returnList = new ArrayList<>();
+        for(int i=0;i<size;i++)
+        {
+            returnList.add(get(i));
+        }
+        return returnList;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size==0;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public T removeFirst() {
+        small();
+        if(size==0)
+            return null;
+        else
+        {int first = Math.floorMod(nextfirst+1,deque.length);
+        T x= deque[first];
+        deque[first] = null;
+        size-=1;
+        nextfirst = first;
+        return x;}
+
+    }
+
+    @Override
+    public T removeLast() {
+        small();
+        if(size==0)
+            return null;
+        else{int last = Math.floorMod(nextlast-1,deque.length);
+        T x = deque[last];
+        deque[last] = null;
+        size-=1;
+        nextlast = last;
+        return x;}
+    }
+
+    @Override
+    public T get(int index) {
+        if(index>=size)
+        {
+            return null;
+        }
+        else
+        {
+            return deque[Math.floorMod(nextfirst+1+index,deque.length)];
+        }
+    }
+
+    @Override
+    public T getRecursive(int index) {
+        throw new UnsupportedOperationException("No need to implement getRecursive for proj1b");
+    }
+}
--- proj1b/tests/ArrayDeque61BTest.java
+++ proj1b/tests/ArrayDeque61BTest.java
@@ -1,10 +1,14 @@
 import deque.ArrayDeque61B;
 
+import deque.Deque61B;
 import jh61b.utils.Reflection;
+import org.checkerframework.checker.units.qual.A;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.Field;
+import java.sql.Array;
+import java.util.Deque;
 import java.util.List;
 
 import static com.google.common.truth.Truth.assertThat;
@@ -12,14 +16,97 @@ import static com.google.common.truth.Truth.assertWithMessage;
 
 public class ArrayDeque61BTest {
 
-//     @Test
-//     @DisplayName("ArrayDeque61B has no fields besides backing array and primitives")
-//     void noNonTrivialFields() {
-//         List<Field> badFields = Reflection.getFields(ArrayDeque61B.class)
-//                 .filter(f -> !(f.getType().isPrimitive() || f.getType().equals(Object[].class) || f.isSynthetic()))
-//                 .toList();
-//
-//         assertWithMessage("Found fields that are not array or primitives").that(badFields).isEmpty();
-//     }
+    @Test
+    public void generatetest(){
+        Deque61B<String> deque1 = new ArrayDeque61B<>();
+        assertThat(deque1.size()).isEqualTo(0);
+        assertThat(deque1.isEmpty()).isTrue();
+    }
+    @Test
+    public void addfirsttest(){
+       Deque61B<String> deque1 = new ArrayDeque61B<>();
+       deque1.addFirst("one");
+       deque1.addFirst("two");
+       deque1.addFirst("three");
+       assertThat(deque1.toList()).containsExactly("three","two","one").inOrder();
+
+    }
+    @Test
+    public void addlasttest(){
+        Deque61B<Integer> deque1 = new ArrayDeque61B<>();
+        deque1.addLast(1);
+        deque1.addLast(2);
+        deque1.addLast(3);
+        assertThat(deque1.toList()).containsExactly(1,2,3).inOrder();
+    }
+    @Test
+    public void addFirstAndAddLastTest(){
+        Deque61B<Integer> deque1 = new ArrayDeque61B<>();
+        deque1.addLast(0);
+        deque1.addLast(1);
+        deque1.addFirst(-1);
+        deque1.addLast(2);
+        deque1.addFirst(-2);
+        assertThat(deque1.toList()).containsExactly(-2,-1,0,1,2).inOrder();
+    }
+    @Test
+    public void isEmpty(){
+        Deque61B<Integer> deque1 = new ArrayDeque61B<>();
+        assertThat(deque1.isEmpty()).isTrue();
+        deque1.addFirst(1);
+        assertThat(deque1.isEmpty()).isFalse();
+    }
+    @Test
+    public void sizeTest(){
+        Deque61B<Integer> deque1 = new ArrayDeque61B<>();
+        assertThat(deque1.size()).isEqualTo(0);
+        deque1.addFirst(1);
+        deque1.addLast(2);
+        assertThat(deque1.size()).isEqualTo(2);
+    }
+    @Test
+    public void reshapeTest(){
+        Deque61B<Integer> deque1 = new ArrayDeque61B<>();
+        for(int i=0;i<14;i++)
+        {
+            deque1.addLast(i);
+        }
+        assertThat(deque1.get(13)).isEqualTo(13);
+        assertThat(deque1.size()).isEqualTo(14);
+
+    }
+    @Test
+    public void getTest(){
+        Deque61B<String> deque1 = new ArrayDeque61B<>();
+        deque1.addLast("front");
+        deque1.addLast("middle");
+        deque1.addLast("back");
+        assertThat(deque1.get(0)).isEqualTo("front");
+        assertThat(deque1.get(1)).isEqualTo("middle");
+        assertThat(deque1.get(2)).isEqualTo("back");
+        assertThat(deque1.get(-1)).isNull();
+        assertThat(deque1.get(255)).isNull();
+    }
+    @Test
+    public void removeFirstTest(){
+        Deque61B<Integer> lld1=  new ArrayDeque61B<>();
+        assertThat(lld1.removeFirst()).isNull();
+        lld1.addFirst(1);
+        lld1.addLast(2);
+        assertThat(lld1.removeFirst()).isEqualTo(1);
+        assertThat(lld1.removeFirst()).isEqualTo(2);
+        assertThat(lld1.removeFirst()).isNull();
+
+    }
+    @Test
+    public void removeLastTest(){
+        Deque61B<Integer> lld1 = new ArrayDeque61B<>();
+        lld1.addFirst(1);
+        lld1.addLast(2);
+        assertThat(lld1.removeLast()).isEqualTo(2);
+        assertThat(lld1.removeLast()).isEqualTo(1);
+        assertThat(lld1.removeLast()).isNull();
+
+    }
 
 }
